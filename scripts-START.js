474279b506b10ba910b1ea75cb46e95549308ccb
Peter 4A5A <498883@edu.rocmn.nl>
2017-09-21 13:16:09 +0200

    Made 20 seconds posible

--- scripts-START.js
+++ scripts-START.js
@@ -4,6 +4,20 @@ var Timer;
 var timeInSeconds;
 var timerTimeout;
 
+var Countdown;
+var remainingTime;
+
+function test() {
+  timeInSeconds = 20;
+  remainingTime = timeInSeconds;
+
+  var sec = Countdown.Seconds();
+  var min = Countdown.Minuuts();
+
+  Countdown.placeTime(min, sec);
+  Timer.start();
+}
+
 (function() {
   Timer = {
     start: function() {
@@ -13,14 +27,48 @@ var timerTimeout;
       clearTimeout(timerTimeout);
     },
     countDown: function() {
-      Timer--;
+      remainingTime--;
       if (Timer.getTimer() == 0) {
         // Stop the timeout on 0
         Timer.stop();
       }
+      var sec = Countdown.Seconds();
+      var min = Countdown.Minuuts();
+
+      Countdown.placeTime(min, sec);
     },
     getTimer: function() {
-      return(timeInSeconds);
+      return(remainingTime);
+    },
+    setTime: function(minuuts) {
+      timeInSeconds = minuuts * 60;
+    }
+  }
+})();
+
+(function() {
+  Countdown = {
+    Minuuts: function() {
+      var minuuts = remainingTime / 60;
+      minuuts = parseInt(minuuts);
+      return(minuuts);
+    },
+
+    Seconds: function() {
+      var minuuts = remainingTime / 60;
+      minuuts = parseInt(minuuts);
+
+      if (minuuts == 0) {
+        return(remainingTime);
+      }
+
+      else {
+        var seconds = remainingTime - seconds;
+        return(seconds);
+      }
+    },
+    placeTime: function(minuuts, seconds) {
+      select(".display__time-left").innerHTML = minuuts + " : " + seconds;
     }
   }
 })();
